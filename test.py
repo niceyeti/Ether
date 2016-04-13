bc3791e3ec543afafeda77bcf1891e53142de6a0
Jesse Waite <jwaite@eecs.wsu.edu>
2016-04-12 21:43:17 -0700

    checkpoint

--- test.py
+++ test.py
@@ -2,12 +2,33 @@ import serpent
 from ethereum import tester, utils, abi
 
 serpent_code = '''
-def multiply(a):
-	return(a*2)
+def foo(a):
+	#I'm a comment! -Ralph Wiggum
+
+	d = array(5)
+	d[0] = 3
+	d[1] = 2
+	d[2] = 2
+	d[3] = 4
+	d[4] = 5
+
+
+	log(a)
+	log(tx.origin)
+	i = 0
+	while i < 10:
+		log(i)
+		i+=1
+
+	return(sha256(d,items=5))
+
 '''
 
 #sets up genesis block of the tester
 s = tester.state()
 c = s.abi_contract(serpent_code)
-o = c.multiply(5)
+#o = c.main(5)
+#print(str(o))
+o = c.foo(3)
 print(str(o))
+
